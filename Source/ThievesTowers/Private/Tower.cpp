5419b9e8851e41704f8c8a0bf559e16e74316cbe
Darkbriks <antoine.patron.62610@gmail.com>
2024-04-23 16:32:15 +0200

    Add sprites and animations for Tower

--- Source/ThievesTowers/Private/Tower.cpp
+++ Source/ThievesTowers/Private/Tower.cpp
@@ -1,6 +1,8 @@
 #include "Tower.h"
 #include "Enemy.h"
 #include "Projectile.h"
+#include "PaperFlipbook.h"
+#include "PaperFlipbookComponent.h"
 #include "Components/CapsuleComponent.h"
 
 //////////////////////////////////////////////////////////////////////////
@@ -17,6 +19,12 @@ ATower::ATower()
 	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
 	CapsuleComponent->SetupAttachment(RootComponent);
 	CapsuleComponent->SetCapsuleSize(AttackRange, 5000.0f);
+	CapsuleComponent->SetGenerateOverlapEvents(true);
+
+	FlipbookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("FlipbookComponent"));
+	FlipbookComponent->SetupAttachment(RootComponent);
+	FlipbookComponent->CastShadow = true;
+	FlipbookComponent->SetFlipbook(IdleAnimation);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -24,13 +32,16 @@ ATower::ATower()
 //////////////////////////////////////////////////////////////////////////
 bool ATower::Attack()
 {
-	TArray<AActor*> OverlappingActors;
-	CapsuleComponent->GetOverlappingActors(OverlappingActors, AEnemy::StaticClass());
-
-	if (OverlappingActors.Num() > 0)
+	if (EnemiesInRange.Num() > 0)
 	{
-		AEnemy* Enemy = Cast<AEnemy>(OverlappingActors[0]);
-		if (AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator))
+		AEnemy* Enemy = EnemiesInRange[0];
+
+		FVector ProjectileLocation = GetActorLocation();
+		ProjectileLocation += GetActorForwardVector() * ProjectileXOffset;
+		ProjectileLocation += GetActorRightVector() * ProjectileYOffset;
+		ProjectileLocation += GetActorUpVector() * ProjectileZOffset;
+		
+		if (AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, ProjectileLocation, FRotator::ZeroRotator))
 		{
 			Enemy->AddProjectile(Projectile);
 			Projectile->InitializeProjectile(Enemy->GetActorLocation(), Enemy);
@@ -41,13 +52,36 @@ bool ATower::Attack()
 	return false;
 }
 
+void ATower::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->IsA(AEnemy::StaticClass()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Enemy Overlap"));
+		EnemiesInRange.Add(Cast<AEnemy>(OtherActor));
+	}
+}
+
+void ATower::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor->IsA(AEnemy::StaticClass()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Enemy End Overlap"));
+		EnemiesInRange.Remove(Cast<AEnemy>(OtherActor));
+		if (EnemiesInRange.Num() <= 0 && FlipbookComponent->GetFlipbook() == AttackAnimation) { FlipbookComponent->SetFlipbook(IdleAnimation); }
+	}
+}
+
 //////////////////////////////////////////////////////////////////////////
 /// ATower - Protected Overrides Methods - AActor
 //////////////////////////////////////////////////////////////////////////
 void ATower::BeginPlay()
 {
 	Super::BeginPlay();
+	AttackAnimationTotalTime = AttackAnimation->GetTotalDuration();
+	FlipbookComponent->SetFlipbook(IdleAnimation);
 	CapsuleComponent->SetCapsuleRadius(AttackRange);
+	CapsuleComponent->OnComponentBeginOverlap.AddDynamic(this, &ATower::BeginOverlap);
+	CapsuleComponent->OnComponentEndOverlap.AddDynamic(this, &ATower::EndOverlap);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -57,6 +91,19 @@ void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	AttackCooldown -= DeltaTime;
-	if (AttackCooldown <= 0.0f) { if (Attack()) { AttackCooldown = AttackSpeed; } }
+	AttackCooldown += DeltaTime;
+	if (AttackCooldown >= AttackSpeed) { if (Attack()) { AttackCooldown = 0.0f; } }
+
+	if (EnemiesInRange.Num() > 0)
+	{
+		AEnemy* Enemy = EnemiesInRange[0];
+		FRotator LookAtRotation = FRotationMatrix::MakeFromX(Enemy->GetActorLocation() - GetActorLocation()).Rotator();
+		SetActorRotation(FRotator(0.0f, LookAtRotation.Yaw, 0.0f));
+	}
+
+	if (FlipbookComponent->GetFlipbook() == AttackAnimation) { AttackAnimationCurrentTime += DeltaTime; }
+	if (AttackAnimationCurrentTime >= AttackAnimationTotalTime) { AttackAnimationCurrentTime = 0.0f; FlipbookComponent->SetFlipbook(IdleAnimation); }
+
+	// Si le temps avant la prochaine attaque est inférieur à ProjectileLaunchTime, on change l'animation
+	if (AttackCooldown >= AttackSpeed - ProjectileLaunchTime && EnemiesInRange.Num() > 0 && FlipbookComponent->GetFlipbook() != AttackAnimation) { FlipbookComponent->SetFlipbook(AttackAnimation); }
 }
--- Source/ThievesTowers/Public/Tower.h
+++ Source/ThievesTowers/Public/Tower.h
@@ -4,8 +4,11 @@
 #include "GameFramework/Actor.h"
 #include "Tower.generated.h"
 
+class AEnemy;
 class UCapsuleComponent;
 class AProjectile;
+class UPaperFlipbook;
+class UPaperFlipbookComponent;
 
 UCLASS()
 class THIEVESTOWERS_API ATower : public AActor
@@ -13,12 +16,18 @@ class THIEVESTOWERS_API ATower : public AActor
 	GENERATED_BODY()
 
 	float AttackCooldown = 0.0f;
+	float AttackAnimationTotalTime = 0.0f;
+	float AttackAnimationCurrentTime = 0.0f;
+	TArray<AEnemy*> EnemiesInRange;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tower - Components", meta = (AllowPrivateAccess = "true"))
 	USceneComponent* SceneComponent;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tower - Components", meta = (AllowPrivateAccess = "true"))
 	UCapsuleComponent* CapsuleComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tower - Components", meta = (AllowPrivateAccess = "true"))
+	UPaperFlipbookComponent* FlipbookComponent;
 
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Attributes")
@@ -30,8 +39,32 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Attributes")
 	float AttackRange = 2000.0f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Attributes")
+	float ProjectileXOffset = 50.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Attributes")
+	float ProjectileYOffset = 5.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Attributes")
+	float ProjectileZOffset = 120.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Animation")
+	UPaperFlipbook* IdleAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Animation")
+	UPaperFlipbook* AttackAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tower - Animation", meta = (ToolTip = "Time in seconds to launch the projectile after the attack animation starts"))
+	float ProjectileLaunchTime = 0.5f;
+	
 	bool Attack();
 
+	UFUNCTION()
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
 	virtual void BeginPlay() override;
 
 public:	
