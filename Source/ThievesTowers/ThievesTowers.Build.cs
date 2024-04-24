554a0a84ea3c56d3e16c32f150857df21d477c33
Darkbriks <antoine.patron.62610@gmail.com>
2024-04-24 21:47:27 +0200

    Update project files

--- Source/ThievesTowers/ThievesTowers.Build.cs
+++ Source/ThievesTowers/ThievesTowers.Build.cs
@@ -8,7 +8,7 @@ public class ThievesTowers : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "EnhancedInput", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
