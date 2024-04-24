b91c0ee9c3af8567441ea04ef7829631c6ca5f51
Darkbriks <antoine.patron.62610@gmail.com>
2024-04-24 21:44:25 +0200

    Add CardHand in GameInstance

--- Source/ThievesTowers/Private/GA_ThievesTowers.cpp
+++ Source/ThievesTowers/Private/GA_ThievesTowers.cpp
@@ -1,5 +1,24 @@
 #include "GA_ThievesTowers.h"
 #include "ProjectileEffect/ProjectileEffect.h"
+#include "Card.h"
+#include "Containers/Ticker.h"
+
+/*void UGA_ThievesTowers::Init()
+{
+	TickDelegateHandle = FTicker::GetCoreTicker().AddTicker(FTickerDelegate::CreateUObject(this, &UGA_ThievesTowers::Tick));
+	Super::Init();
+}
+
+void UGA_ThievesTowers::Shutdown()
+{
+	FTicker::GetCoreTicker().RemoveTicker(TickDelegateHandle);
+	Super::Shutdown();
+}*/
+
+void UGA_ThievesTowers::Tick(float DeltaTime)
+{
+	//UE_LOG(LogTemp, Warning, TEXT("UGA_ThievesTowers::Tick"));
+}
 
 UProjectileEffect* UGA_ThievesTowers::GetProjectileEffect(TSubclassOf<UProjectileEffect> EffectClass)
 {
@@ -12,4 +31,21 @@ UProjectileEffect* UGA_ThievesTowers::GetProjectileEffect(TSubclassOf<UProjectil
 	UProjectileEffect* NewEffect = NewObject<UProjectileEffect>(this, EffectClass);
 	ProjectileEffects.Add(NewEffect);
 	return NewEffect;
+}
+
+void UGA_ThievesTowers::AddCardToHand(TSubclassOf<ACard> CardClass)
+{
+	if (Hand.Num() >= MaxHandSize) { return; }
+	Hand.Add(CardClass);
+	
+	if (CreateHandDelegate.ExecuteIfBound(Hand)) { UE_LOG(LogTemp, Warning, TEXT("CreateHandDelegate executed")); }
+	else { UE_LOG(LogTemp, Warning, TEXT("CreateHandDelegate not executed")); }
+}
+
+void UGA_ThievesTowers::RemoveCardFromHand(TSubclassOf<ACard> CardClass)
+{
+	for (int i = 0; i < Hand.Num(); i++) { if (Hand[i] == CardClass) { Hand.RemoveAt(i); break; } }
+	
+	if (CreateHandDelegate.ExecuteIfBound(Hand)) { UE_LOG(LogTemp, Warning, TEXT("CreateHandDelegate executed")); }
+	else { UE_LOG(LogTemp, Warning, TEXT("CreateHandDelegate not executed")); }
 }
--- Source/ThievesTowers/Public/GA_ThievesTowers.h
+++ Source/ThievesTowers/Public/GA_ThievesTowers.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TD_Player.h"
 #include "Engine/GameInstance.h"
 #include "GA_ThievesTowers.generated.h"
 
 class UProjectileEffect;
+class ACard;
+
+
 
 UCLASS()
 class THIEVESTOWERS_API UGA_ThievesTowers : public UGameInstance
@@ -13,6 +17,25 @@ class THIEVESTOWERS_API UGA_ThievesTowers : public UGameInstance
 
 	TArray<UProjectileEffect*> ProjectileEffects;
 
+	int MaxHandSize = 8;
+	TArray<TSubclassOf<ACard>> Hand;
+
 public:
+	// Methods
+	void Init() override;
+	void Shutdown() override;
+	void Tick(float DeltaTime);
+
+	// Delegates
+	FCreateHandDelegate CreateHandDelegate;
+	FDelegateHandle TickDelegateHandle;
+
+	// Getters
 	UProjectileEffect* GetProjectileEffect(TSubclassOf<UProjectileEffect> EffectClass);
+	int GetMaxHandSize() { return MaxHandSize; }
+	TArray<TSubclassOf<ACard>> GetHand() { return Hand; }
+
+	UFUNCTION(BlueprintCallable)
+	void AddCardToHand(TSubclassOf<ACard> CardClass);
+	void RemoveCardFromHand(TSubclassOf<ACard> CardClass);
 };
